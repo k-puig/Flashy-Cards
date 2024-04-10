c814daf7488e9b75f91e950b12fb4309c5c8c053
Kevin Puig <119972216+k-puig@users.noreply.github.com>
2024-04-10 14:37:49 -0400

    Update FlashCardQuizBuilder.java

--- app/src/main/java/com/kpuig/flashycards/cards/quiz/FlashCardQuizBuilder.java
+++ app/src/main/java/com/kpuig/flashycards/cards/quiz/FlashCardQuizBuilder.java
@@ -1,16 +1,19 @@
 package com.kpuig.flashycards.cards.quiz;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import com.kpuig.flashycards.cards.FlashCard;
 
 public class FlashCardQuizBuilder {
+    private static Random random = new Random();
+
     private FlashCardQuizBuilder() {
         // No body here
     }
 
     public static FlashCardQuiz createNewRandomQuiz(ArrayList<FlashCard> cards) {
-        // TODO: implement this method
+        // TODO: implement this method using static random field
         throw new UnsupportedOperationException("Method not yet implemented");
     }
 }
