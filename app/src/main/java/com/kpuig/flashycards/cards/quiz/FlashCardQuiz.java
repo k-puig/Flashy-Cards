170da015c75ae31c9316e2180dea0189d54157cf
Mandy Saint Simon <mandysaintsimon@gmail.com>
2024-04-11 14:57:04 -0400

    Update FlashCardQuiz.java

--- app/src/main/java/com/kpuig/flashycards/cards/quiz/FlashCardQuiz.java
+++ app/src/main/java/com/kpuig/flashycards/cards/quiz/FlashCardQuiz.java
@@ -19,8 +19,24 @@ public class FlashCardQuiz {
     // Inclusive Range [0.0, 1.0]
     public double getScore() {
         // TODO: Implement this method such that the highest score is 1.0 (100%) and lowest is 0.0 (0%)
-        throw new UnsupportedOperationException("Unimplemented method");
-    }
+        
+        int totalQuestions = questions.size();
+        if(totalQuestions == 0 ){
+            return 0.0;
+        }
+
+        int correctAnswers = 0;// counts the correct answers
+        for(Question question : questions){// iterate through question for correct answers
+            if(question.isCorrect()){
+                correctAnswers++;
+            }//end if
+        }// end for
+
+        return (double) correctAnswers /totalQuestions;// return score
+
+       // throw new UnsupportedOperationException("Unimplemented method");
+       
+    }//end of get score
 
     public void answerQuestion(int questionIndex, int answer) {
         // TODO: Implement this method
