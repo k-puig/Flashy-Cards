7b47ab77d3b12b22d9bb5a5bb839f0a287116f87
Beselik <137002524+Beselik@users.noreply.github.com>
2024-04-15 22:32:02 -0400

    Json stuff

--- app/src/main/java/com/kpuig/flashycards/App.java
+++ app/src/main/java/com/kpuig/flashycards/App.java
@@ -3,12 +3,35 @@
  */
 package com.kpuig.flashycards;
 
+import com.kpuig.flashycards.cards.FlashCard;
+import com.kpuig.flashycards.cards.storage.FlashCardSet;
 import com.kpuig.flashycards.ui.MainApplication;
 
 import javafx.application.Application;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
+        File jsonFile = new File("flashcards.json");
+        String sampleJson = "[{\"front\":\"Question 1\",\"back\":\"Answer 1\"},{\"front\":\"Question 2\",\"back\":\"Answer 2\"}]";
+
+       
+        FlashCardSet flashCardSet = new FlashCardSet(sampleJson);
+
+        
+        List<FlashCard> cards = flashCardSet.getCards();
+
+        
+        for (int i = 0; i < cards.size(); i++) {
+            FlashCard card = cards.get(i);
+            System.out.println("Flashcard " + (i + 1) + ":");
+            System.out.println(card); // This calls the overridden toString() method
+            System.out.println();
+        }
+        
         Application.launch(MainApplication.class, args);
+        
     }
 }
--- app/src/main/java/com/kpuig/flashycards/cards/storage/FlashCardSet.java
+++ app/src/main/java/com/kpuig/flashycards/cards/storage/FlashCardSet.java
@@ -2,7 +2,15 @@ package com.kpuig.flashycards.cards.storage;
 
 import java.io.File;
 import java.util.List;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.io.FileWriter;
 
+
+import com.google.gson.Gson;
+import com.google.gson.internal.bind.util.ISO8601Utils;
 import com.kpuig.flashycards.cards.FlashCard;
 
 // This class may have limited functionality in prototyping stage
@@ -13,15 +21,38 @@ public class FlashCardSet {
         this.cards = cards;
     }
 
-    public FlashCardSet(File cardSetFile) {
-        // TODO: Maybe implement
+    public FlashCardSet(File cardSetFile) throws IOException {
+        // Handle potential IOException when reading from file
+        try {
+            Gson gson = new Gson();
+            try (FileReader reader = new FileReader(cardSetFile)) {
+                FlashCard[] flashCards = gson.fromJson(reader, FlashCard[].class);
+                this.cards = Arrays.asList(flashCards);
+            }
+        } catch (IOException e) {
+            // Propagate the exception or handle it accordingly
+            throw e;
+        }
+    }
+    public FlashCardSet(String jsonString) {
+        Gson gson = new Gson();
+        FlashCard[] flashCards = gson.fromJson(jsonString, FlashCard[].class);
+        this.cards = Arrays.asList(flashCards);
     }
 
-    public void saveToFile(File file) {
+    public void saveToFile(File file) throws IOException {
+        Gson gson = new Gson();
+        try(FileWriter writer = new FileWriter(file)){
+            gson.toJson(this.cards, writer);
+        }
         // TODO: Maybe implement
+        // Gson gson = new Gson();
+        // System.out.println(gson.toJson(gson));
+        // Implement a method to save the json as a string to the file.
     }
 
     public List<FlashCard> getCards() {
         return this.cards;
     }
 }
+
